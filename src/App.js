3c19efeff7382c8f6cb078e91149f3da6403e409
Mark Lee <mark@protopie.io>
2020-12-28 10:53:44 +0900

    fds17

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import Example1 from "./components/Example1";
+import Example2 from "./components/Example2";
+import Example3 from "./components/Example3";
+
+import { Provider } from "./contexts/PersonContext";
 
 function App() {
+  const [persons, setPersons] = useState([
+    { id: 0, name: "Mark", age: 38 },
+    { id: 1, name: "Hanna", age: 27 },
+  ]);
+
+  function add() {
+    setPersons([...persons, { id: 2, name: "React", age: 10 }]);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider value={{ persons, add }}>
+      <div className="App">
+        <header className="App-header">
+          <Example1 />
+          <Example2 />
+          <Example3 />
+        </header>
+      </div>
+    </Provider>
   );
 }
 
